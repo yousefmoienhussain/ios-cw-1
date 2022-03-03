a03fc5630d5b9225616320690e11db4aa1415995
yousef moien hussain <zeroyousef13@gmail.com>
2022-03-03 18:17:12 +0300

    yousef moien

---
+++ cw2-day1/cw2-day1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cw2-day1
+//
+//  Created by Mac on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello,yousef!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw2-day1/cw2-day1/cw2_day1App.swift
@@ -0,0 +1,17 @@
+//
+//  cw2_day1App.swift
+//  cw2-day1
+//
+//  Created by Mac on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw2_day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
